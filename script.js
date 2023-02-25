564f42d07a3cbadbfab397b54d72f65116eb55f2
kuldeepsaini23 <kuldeepsaini02311@gmail.com>
2023-02-25 21:57:29 +0530

    Some css changes

--- script.js
+++ script.js
@@ -23,3 +23,43 @@ const openFollowModal = () => {
 };
 
 
+
+// Copy to clipboard
+function copyLinkToClipboard() {
+  const websiteUrl = "https://kuldeepsaini23.github.io/ProfileModal/"; // Replace with your website URL
+  const copyText = document.createElement("textarea");
+  copyText.value = websiteUrl;
+  document.body.appendChild(copyText);
+  copyText.select();
+  document.execCommand("copy");
+  document.body.removeChild(copyText);
+  const toastMessage = document.createElement("div");
+  toastMessage.innerHTML = "Link copied to clipboard!";
+  document.body.appendChild(toastMessage);
+  setTimeout(function() {
+    document.body.removeChild(toastMessage);
+  }, 2000);
+}
+
+async function alert() {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'bottom-left',
+    iconColor: 'white',
+    customClass: {
+      popup: 'colored-toast'
+    },
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true
+  })
+  await Toast.fire({
+    icon: 'success',
+    title: 'Link Copied to Clipboard'
+  })
+}
+
+
+//! Some Js function to copy to clip board
+
+// <a href="#" onclick="navigator.clipboard.writeText('https://example.com').then(function() { alert('Link copied to clipboard!'); }).catch(function() { alert('Failed to copy link to clipboard!'); }); return false;">Copy Link</a>
